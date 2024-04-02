0b8636dd4e23c88c1ed5c5fad780c64ef3f45ff8
kunal <151952000+kunnnal@users.noreply.github.com>
2024-04-02 16:14:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+let input = document.getElementById("display");
+function AddNumber(numbers) {
+  input.value += numbers;
+}
+function AddOperator(operator) {
+  input.value += operator;
+}
+function Clear() {
+  input.value = "";
+}
+function Calculate() {
+  try {
+    const result = calculator(input.value);
+    input.value = result;
+  } catch (error) {
+    input.value = "Error";
+  }
+}
+
+function calculator(expression) {
+  const operators = expression.split(/[0-9.]+/).filter((op) => op);
+  const operands = expression.split(/[\+\-\*\/]/).map(Number);
+
+  let result = operands[0];
+
+  for (let i = 0; i < operators.length; i++) {
+    const operator = operators[i];
+    const nextOperand = operands[i + 1];
+
+    switch (operator) {
+      case "+":
+        result += nextOperand;
+        break;
+      case "-":
+        result -= nextOperand;
+        break;
+      case "*":
+        result *= nextOperand;
+        break;
+      case "/":
+        if (nextOperand === 0) {
+          throw new error("undefine");
+        }
+        result /= nextOperand;
+        break;
+      default:
+        throw new Error("Invalid operator");
+    }
+  }
+
+  return result;
+}
+function Sqrt() {
+  console.log("Sqrt");
+  const value = parseFloat(input.value);
+  if (value >= 0) {
+    const sqrtResult = Math.sqrt(value);
+    input.value = sqrtResult;
+  } else {
+    input.value = " ";
+  }
+}
